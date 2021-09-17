812675035a35ec64eae6adabc03e212e68ec8825
Mike Andrews <mike.andrews@alum.wpi.edu>
2021-09-16 20:49:21 -0400

    Correct alembic's env.py registry import.

--- jasmine-webserver/migrations/env.py
+++ jasmine-webserver/migrations/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from jasmine_webserver.models import orm_registry
+from jasmine.webserver.models import orm_registry
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
