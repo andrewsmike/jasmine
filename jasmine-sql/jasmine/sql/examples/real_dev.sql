9d024522e911ab25a2fb3d5513c8a573762b4a2a
Mike Andrews <mike.andrews@alum.wpi.edu>
2021-10-16 13:44:57 -0400

    JasmineSQL: Add more example queries.

---
+++ jasmine-sql/jasmine/sql/examples/real_dev.sql
@@ -0,0 +1,18 @@
+SELECT organization.name as Organization,
+       CONCAT("[", project.name, "]") as Project,
+       `view`.path as Path,
+       CAST(AVG(LENGTH(`view`.spec->>"$.query_text")) AS SIGNED) AS `Query length`,
+       COUNT(DISTINCT backend_event.backend_event_id) AS Events,
+       COUNT(DISTINCT `user`.user_id) AS `Possible users`
+  FROM views `view`
+  LEFT JOIN projects project
+    ON `view`.project_id = project.project_id
+  LEFT JOIN backend_events backend_event
+    ON `view`.view_id = backend_event.view_id
+  LEFT JOIN organizations organization
+    ON project.organization_id = organization.organization_id
+  LEFT JOIN users `user`
+    ON user.organization_id = organization.organization_id
+ GROUP BY 1, 2, 3
+ ORDER BY 1, 2, 3;
+
---
+++ jasmine-sql/jasmine/sql/examples/sum.sql
@@ -0,0 +1,2 @@
+SELECT SUM(1) FROM my_table;
+SELECT COUNT(DISTINCT organization.organization_id) FROM organizations;
