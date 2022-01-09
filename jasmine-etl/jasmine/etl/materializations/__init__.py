0631f3684dee8a8e4ac8261db53af76535e80580
Mike Andrews <mike.andrews@alum.wpi.edu>
2022-01-08 19:35:19 -0500

    Have ETLs export their resource_names(mat) functions for debugging tools.

--- jasmine-etl/jasmine/etl/materializations/__init__.py
+++ jasmine-etl/jasmine/etl/materializations/__init__.py
@@ -1,7 +1,15 @@
-from jasmine.etl.materializations.upsert import upsert_event_funcs
-from jasmine.etl.materializations.view import view_event_funcs
+from jasmine.etl.materializations.upsert import (
+    upsert_event_funcs,
+    upsert_resource_names,
+)
+from jasmine.etl.materializations.view import view_event_funcs, view_resource_names
 
 materialization_event_funcs = {
     "view": view_event_funcs,
     "upsert": upsert_event_funcs,
+}
+
+materialization_resource_names_funcs = {
+    "view": view_resource_names,
+    "upsert": upsert_resource_names,
 }
