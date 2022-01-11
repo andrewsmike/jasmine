314ef0391770b87251225b2b6038312e96919d9a
Mike Andrews <mike.andrews@alum.wpi.edu>
2022-01-11 17:29:31 -0500

    tools/grep.sh: Properly quote arguments.

--- tools/grep.sh
+++ tools/grep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export EXCLUDE_DIRS="--exclude-dir=node_modules --exclude-dir=__pycache__ --exclude-dir=.mypy_cache --exclude-dir=\.pytest_cache --exclude-dir=build"
 
-grep $EXCLUDE_DIRS -rie $@
+grep $EXCLUDE_DIRS -rie "$@"
