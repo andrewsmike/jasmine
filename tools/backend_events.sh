a9b0874e0d7f30be0c857d6571ffc7b8352e6fd7
Mike Andrews <mike.andrews@alum.wpi.edu>
2022-01-11 20:53:13 -0500

    Tools: Limit output size, don't report SQL dump data.

--- tools/backend_events.sh
+++ tools/backend_events.sh
@@ -1,14 +1,14 @@
 # Note: You may need to set your mysql client's default timezone.
 
-INTERVAL="5 MINUTE"
+INTERVAL="2 MINUTE"
 
 for DB in web test
 do
-    echo "SELECT * FROM jasmine_${DB}.backend_events WHERE created_time > DATE_SUB(NOW(), INTERVAL $INTERVAL) ORDER BY created_time ASC\G" | \
+    echo "SELECT * FROM (SELECT * FROM jasmine_${DB}.backend_events WHERE created_time > DATE_SUB(NOW(), INTERVAL $INTERVAL) ORDER BY created_time DESC LIMIT 10) t ORDER BY created_time ASC\G" | \
         mysql \
             --user=jasmine_web_su \
             --password=password \
             --host=127.0.0.1 \
             --port=3305 \
-            jasmine_web
+            jasmine_${DB}
 done
