895ee5ba728a6db72152f522939fa4fcdc7822e5
Mike Andrews <mike.andrews@alum.wpi.edu>
2022-01-14 23:04:42 -0500

    ./tools/grep.sh: Hide package*.json.

--- tools/grep.sh
+++ tools/grep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export EXCLUDE_DIRS="--exclude-dir=node_modules --exclude-dir=__pycache__ --exclude-dir=.mypy_cache --exclude-dir=\.pytest_cache --exclude-dir=build"
+export EXCLUDE_DIRS="--exclude-dir=node_modules --exclude-dir=__pycache__ --exclude-dir=.mypy_cache --exclude-dir=\.pytest_cache --exclude-dir=build --exclude=package.json --exclude=package-lock.json"
 
 grep $EXCLUDE_DIRS -rie "$@" | grep -ve 'dump.sql'
